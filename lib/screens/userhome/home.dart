ae9b5cf3b4bd59faf1c8cc5e79689cc6249d1956
Abdullah khan <abdullah234ktk@gmail.com>
2020-03-31 01:34:16 +0500

    firebase push notification implementation

--- lib/screens/userhome/home.dart
+++ lib/screens/userhome/home.dart
@@ -1,9 +1,11 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:liderfacilites/models/User.dart';
 import 'package:liderfacilites/models/app_localization.dart';
 import 'package:liderfacilites/models/firestore.dart';
-import 'package:liderfacilites/models/utils.dart';
 import 'package:liderfacilites/screens/taskerhome/taskernav.dart';
 import 'package:liderfacilites/screens/userhome/first_page.dart';
 import 'package:liderfacilites/screens/userhome/second_page.dart';
@@ -19,11 +21,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-//   final _navigatorKey = GlobalKey<NavigatorState>();
-
-//   static const TextStyle optionStyle =
-//       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-
   var useruid;
   _HomePageState(this.useruid);
 
@@ -31,6 +28,10 @@ class _HomePageState extends State<HomePage> {
   final db = Firestore.instance;
   //user record
   User _userRecord;
+
+  //messaging
+  final Firestore _db = Firestore.instance;
+  final FirebaseMessaging _fcm = FirebaseMessaging();
 
   @override
   void initState() {
@@ -40,205 +41,108 @@ class _HomePageState extends State<HomePage> {
         publishableKey: "pk_test_hTtMDzqHYQp6R6NnFXroNpO9001VoFKriY",
         merchantId: "Test",
         androidPayMode: 'test'));
+
+    //messages
+    _saveDeviceToken();
+    getMessage();
   }
 
-//   final List<Widget> pages = [
-//     FirstPage(
-//       key: PageStorageKey('Page1'),
-//     ),
-//     SecondPage(
-//       key: PageStorageKey('Page2'),
-//     ),
-//     ThirdPage(
-//       key: PageStorageKey('Page3'),
-//     ),
-//     FourthPage(
-//       key: PageStorageKey('Page4'),
-//     ),
-//     Center(child: Text('Switch')),
-//   ];
+  _saveDeviceToken() async {
+    // Get the current user
+    // String uid = 'jeffd23';
+    // FirebaseUser user = await _auth.currentUser();
 
-//   final PageStorageBucket bucket = PageStorageBucket();
+    // Get the token for this device
+    String fcmToken = await _fcm.getToken();
 
-//   int _selectedIndex = 0;
+    // Save it to Firestore
+    if (fcmToken != null) {
+      var tokens = _db
+          .collection('users')
+          .document(useruid)
+          .collection('tokens')
+          .document(fcmToken);
 
-//   @override
-//   void initState() {
-//     // loadCurrentUserData();
-//     super.initState();
-//   }
+      await tokens.setData({
+        'token': fcmToken,
+        // 'createdAt': FieldValue.serverTimestamp(), // optional
+        // 'platform': Platform.operatingSystem // optional
+      });
+    }
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         body:
-//             //   WillPopScope(
-//             //   onWillPop: () async {
-//             //     if (_navigatorKey.currentState.canPop()) {
-//             //       _navigatorKey.currentState.pop();
-//             //       return false;
-//             //     }
-//             //     return true;
-//             //   },
-//             //   child: Navigator(
-//             //     key: _navigatorKey,
-//             //     initialRoute: '/',
-//             //     onGenerateRoute: (RouteSettings settings) {
-//             //       WidgetBuilder builder;
-//             //       // Manage your route names here
-//             //       switch (settings.name) {
-//             //         case '/':
-//             //           builder = (BuildContext context) => FirstPage();
-//             //           break;
-//             //         case '/page2':
-//             //           builder = (BuildContext context) => SecondPage();
-//             //           break;
-//             //         case '/page3':
-//             //           builder = (BuildContext context) => ThirdPage();
-//             //           break;
-//             //         case '/page4':
-//             //           builder = (BuildContext context) => FourthPage();
-//             //           break;
-//             //         default:
-//             //           throw Exception('Invalid route: ${settings.name}');
-//             //       }
-//             //       // You can also return a PageRouteBuilder and
-//             //       // define custom transitions between pages
+  getMessage() {
+    _fcm.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        // final snackbar = SnackBar(
+        //   content: Text(message['notification']['title']),
+        //   action: SnackBarAction(
+        //     label: 'Go',
+        //     onPressed: () => null,
+        //   ),
+        // );
 
-//             //       return MaterialPageRoute(
-//             //         builder: builder,
-//             //         settings: settings,
-//             //       );
-//             //     },
-//             //   ),
-//             // ), bottomNavigationBar: bottomNaviationBar,);
-
-//             StreamBuilder(
-//                 stream: Firestore.instance
-//                     .collection('users')
-//                     .document(useruid)
-//                     .snapshots(),
-//                 builder: (context, snapshot) {
-//                   if (!snapshot.hasData) return SplashScreen();
-
-//                   // final record = User.fromSnapshot(snapshot.data);
-//                   updateUserData(snapshot.data);
-//                   return IndexedStack(
-//                     index: _selectedIndex,
-//                     children: <Widget>[
-//                       bodycontent(),
-//                       // Positioned(
-//                       //   bottom: 0,
-//                       //   left: 0,
-//                       //   right: 0,
-//                       //   child: bottomNaviationBar,
-//                       // ),
-//                     ],
-//                   );
-
-//                 }),bottomNavigationBar: bottomNaviationBar,);
-//   }
-
-//   bodycontent() {
-//     return Container(
-//       child: pages[_selectedIndex],
-//     );
-//   }
-
-//   Widget get bottomNaviationBar {
-//     return ClipRRect(
-//         borderRadius: BorderRadius.only(
-//             topRight: Radius.circular(25), topLeft: Radius.circular(25)),
-//         child: Theme(
-//           data: Theme.of(context).copyWith(
-//               // sets the background color of the `BottomNavigationBar`
-//               canvasColor: Colors.white,
-//               // sets the active color of the `BottomNavigationBar` if `Brightness` is light
-//               primaryColor: Colors.white,
-//               textTheme: Theme.of(context).textTheme.copyWith(
-//                   caption: new TextStyle(
-//                       color: Colors
-//                           .white))), // sets the inactive color of the `BottomNavigationBar`
-//           child: BottomNavigationBar(
-//             type: BottomNavigationBarType.fixed,
-//             showUnselectedLabels: true,
-//             items: <BottomNavigationBarItem>[
-//               BottomNavigationBarItem(
-//                 icon: Icon(
-//                   Icons.explore,
-//                   size: 32,
-//                   color: getColor(0),
-//                 ),
-//                 title: Text(
-//                   AppLocalizations.of(context).translate('Explore'),
-//                   style: TextStyle(color: Colors.black45),
-//                 ),
-//               ),
-//               BottomNavigationBarItem(
-//                 icon: Icon(
-//                   Icons.book,
-//                   size: 30,
-//                   color: getColor(1),
-//                 ),
-//                 title: Text(
-//                   AppLocalizations.of(context).translate('Booking'),
-//                   style: TextStyle(color: Colors.black45),
-//                 ),
-//               ),
-//               BottomNavigationBarItem(
-//                 icon: Icon(
-//                   Icons.favorite,
-//                   size: 30,
-//                   color: getColor(2),
-//                 ),
-//                 title: Text(
-//                   AppLocalizations.of(context).translate('Saved'),
-//                   style: TextStyle(color: Colors.black45),
-//                 ),
-//               ),
-//               BottomNavigationBarItem(
-//                 icon: Icon(Icons.account_circle, size: 30, color: getColor(3)),
-//                 title: Text(
-//                   AppLocalizations.of(context).translate('Account'),
-//                   style: TextStyle(color: Colors.black45),
-//                 ),
-//               ),
-//               BottomNavigationBarItem(
-//                 icon: Icon(
-//                   Icons.swap_horizontal_circle,
-//                   size: 30,
-//                   color: getColor(4),
-//                 ),
-//                 title: Text(
-//                   AppLocalizations.of(context).translate('Switch'),
-//                   textAlign: TextAlign.center,
-//                   style: TextStyle(color: Colors.black45),
-//                 ),
-//               ),
-//             ],
-//             currentIndex: _selectedIndex,
-//             selectedItemColor: Colors.blue,
-//             onTap: _onItemTapped,
-//           ),
-//         ));
-//   }
-
-//   void _onItemTapped(int index) {
-//     setState(() {
-//       _selectedIndex = index;
-//       // if (index == 1) _navigatorKey.currentState.pushNamed('/page2');
-//       // if (index == 2) _navigatorKey.currentState.pushNamed('/page3');
-//       // if (index == 3) _navigatorKey.currentState.pushNamed('/page4');
-//     });
-//   }
-
-//   getColor(int index) {
-//     if (index == _selectedIndex) {
-//       return Colors.blue;
-//     } else {
-//       return Colors.black38;
-//     }
-//   }
+        // Scaffold.of(context).showSnackBar(snackbar);
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: ListTile(
+              title: Text(message['notification']['title']),
+              subtitle: Text(message['notification']['body']),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                color: Colors.amber,
+                child: Text('Ok'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        // TODO optional
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: ListTile(
+              title: Text(message['notification']['title']),
+              subtitle: Text(message['notification']['body']),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                color: Colors.amber,
+                child: Text('Ok'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        // TODO optional
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: ListTile(
+              title: Text(message['notification']['title']),
+              subtitle: Text(message['notification']['body']),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                color: Colors.amber,
+                child: Text('Ok'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   updateUserData(DocumentSnapshot snapshot) async {
     debugPrint('update user data');
@@ -264,26 +168,6 @@ class _HomePageState extends State<HomePage> {
     if (_userRecord.requestList != null)
       user.setRequestList(_userRecord.requestList);
   }
-
-//   // loadCurrentUserData() async {
-//   //   await db
-//   //       .collection('users')
-//   //       .document(useruid)
-//   //       .get()
-//   //       .then((DocumentSnapshot snapshot) {
-//   //     debugPrint('before');
-//   //     userRecord = User.fromSnapshot(snapshot);
-//   //     User user = new User();
-//   //     user.setUID(useruid);
-//   //     user.setname(userRecord.name);
-//   //     user.setEmail(userRecord.email);
-//   //     user.setPhoneNum(userRecord.phoneNumber);
-//   //     user.setUserState(userRecord.isTasker);
-//   //     debugPrint('us: ' + userRecord.isTasker.toString());
-//   //     if (userRecord.imageUrl != null) user.setImageUrl(userRecord.imageUrl);
-//   //   });
-//   // }
-// }
 
   final List<Widget> pages = [
     FirstPage(
